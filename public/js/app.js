d14c1178197908c80e1e8c450606eff46c927fa9
Saeed-Altout <saeedaltoutpro@gmail.com>
2023-09-27 18:08:20 +0300

    Added btn logout in layout

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,8 @@
 
 const toggle = document.querySelector("#toggle");
 const menuMobile = document.querySelector("#menuMobile");
+const name = document.querySelector("#profile-name");
+const logout = document.querySelector("#btn-logout");
 
 // // On Page Loaded
 // window.onload = () => {
@@ -13,4 +15,11 @@ toggle.addEventListener("click", () => {
     console.log("clicked");
     console.log(menuMobile);
     menuMobile.classList.toggle("translate-x-[100%]");
+});
+
+window.onload = () => {
+    logout.classList.toggle("hidden");
+};
+name.addEventListener("click", () => {
+    logout.classList.toggle("hidden");
 });
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,12 +19,12 @@
      <header class="bg-zinc-900 h-[76px] fixed w-full top-0 z-50">
           <nav class="container flex items-center justify-between px-10 py-6 mx-auto">
                <a class="font-semibold text-white " href="{{ route('home.index') }}">E-Commerce</a>
-               <ul class="items-center justify-between hidden gap-10 list-none md:flex">
+               <ul class="items-center justify-between hidden gap-10 list-none md:flex ml-auto">
                     <li><a class="font-semibold text-gray-400 duration-300 ease-in-out hover:text-white" href="{{ route('home.index') }}">Home</a></li>
                     <li><a class="font-semibold text-gray-400 duration-300 ease-in-out hover:text-white" href="{{ route('cart.index') }}">Cart</a></li>
                     <li><a class="font-semibold text-gray-400 duration-300 ease-in-out hover:text-white" href="{{ route('products.index') }}">Products</a></li>
                </ul>
-               <button class="block text-white md:hidden w-fit" id="toggle">
+               <button class="block ml-auto text-white md:hidden w-fit" id="toggle">
                     <img src="{{ url('images/menu.svg') }}" alt="menu">
                </button>
                <ul id="menuMobile" class="absolute duration-300 ease-in-out right-0 translate-x-[100%]  h-screen transition-transform top-[75px] bg-zinc-900 w-72 p-10 flex flex-col items-center justify-start gap-10 list-none md:hidden">
@@ -32,6 +32,15 @@
                     <li><a class="font-semibold text-gray-400 duration-300 ease-in-out hover:text-white" href="{{ route('cart.index') }}">Cart</a></li>
                     <li><a class="font-semibold text-gray-400 duration-300 ease-in-out hover:text-white" href="{{ route('products.index') }}">Products</a></li>
                </ul>
+
+               <div class="ml-4 text-white relative">
+                    <p id="profile-name" class="font-semibold text-gray-400 duration-300 ease-in-out hover:text-white cursor-pointer">Saeed Altout</p>
+                    <div id="btn-logout" class="absolute top-10 bg-zinc-900 px-5 py-10 right-0 rounded-lg">
+                         <button class="hover:underline font-semibold duration-200 ease-linear ">
+                              Logout
+                         </button>
+                    </div>
+               </div>
           </nav>
      </header>
 
